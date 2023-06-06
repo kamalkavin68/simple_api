2361f919328e4d314cb0b2d39f6e0fdf38b0d8cf
Kavin <Kavi448SU15@coral.com>
2023-06-06 15:25:18 +0530

    test

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = FastAPI()
 def home():
     return {"message":"API running..."}
 
+@app.get("/user")
+def user():
+    return {"user": "kavin"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
