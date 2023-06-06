b3ef207a619a669a5a7b444c6edb9a27679853ed
Kavin <Kavi448SU15@coral.com>
2023-06-06 15:33:20 +0530

    test

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
-
-from get_data import get_live_price
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message":"API running..."}
 
-@app.post("/get_price")
-def user(symbol:str):
-    return get_live_price(symbol)
+@app.get("/user")
+def user():
+    return {"user": "kavin"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
